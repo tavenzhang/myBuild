1f30f968dda8c36c5d25edcadb47665675a8311e
taven-jx <taven-jx@jxinternet.com>
2018-10-11 10:51:28 +0800

    no message

---
+++ ipa_build/ipa.sh
@@ -0,0 +1,27 @@
+#ipa 资源替换
+scriptPath=$(cd `dirname $0`; pwd)
+outPut=${scriptPath}/outPut
+srcIpa=${scriptPath}/src.ipa
+targetIpa=new.ipa
+replaceDir=replace
+resRoot=$outPut/Payload/JD.app
+
+
+mkdir -p $outPut
+
+rm  -rf $outPut/*
+
+unzip -q $srcIpa -d $outPut
+cp -rf  $replaceDir/  $resRoot
+#xcrun -sdk iphoneos PackageApplication -v "${resRoot}" -o  "${outPut}/new.ipa"
+#dg deploy ${outPutDir}/${ipaName}
+cd  $outPut
+zip -qr $targetIpa   *
+#dg deploy $targetIpa
+open .
+
+
+
+
+
+
